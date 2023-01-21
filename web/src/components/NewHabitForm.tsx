c74542a825e1b237ca5ae299468ef000af3345a2
Pedro <pedropaivadev@gmail.com>
2023-01-21 20:38:00 -0300

    patch method to change habits checkbox and progressbar. Transitions and focus styles

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -11,7 +11,6 @@ export function NewHabitForm() {
 
     async function createNewHabit(event: FormEvent) {
         event.preventDefault()
-        console.log(title, weekDays)
 
         if (!title || weekDays.length === 0) {
             return
@@ -45,7 +44,7 @@ export function NewHabitForm() {
                 type="text"
                 id="title"
                 placeholder="ex.: Exercícios, dormir bem, etc..."
-                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
                 autoFocus
                 value={title}
                 onChange={event => setTitle(event.target.value)}
@@ -57,11 +56,11 @@ export function NewHabitForm() {
                 {availableWeekDays.map((weekDay, index) => 
                     <Checkbox.Root
                         key={weekDay}
-                        className="flex items-center gap-3 group"
+                        className="flex items-center gap-3 group focus:outline-none"
                         checked={weekDays.includes(index)}
                         onCheckedChange={() => handleToggleWeekDay(index)}
                     >
-                        <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                        <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background">
                             <Checkbox.Indicator>
                                 <Check size={20} className="text-white"/>
                             </Checkbox.Indicator>
@@ -70,7 +69,10 @@ export function NewHabitForm() {
                     </Checkbox.Root>
                 )}
             </div>
-            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+            <button 
+                type="submit" 
+                className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+            >
                 <Check size={20} weight="bold"/>
                 Confirmar
             </button>
